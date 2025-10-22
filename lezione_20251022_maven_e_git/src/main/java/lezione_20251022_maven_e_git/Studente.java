2ccae3c39f236c6bcdb2b29831ef66224f33d0a7
Exolab <f.protano@exolab.it>
2025-10-22 11:00:43 +0200

    ho aggiunto la classe studente

--- lezione_20251022_maven_e_git/src/main/java/lezione_20251022_maven_e_git/Studente.java
+++ lezione_20251022_maven_e_git/src/main/java/lezione_20251022_maven_e_git/Studente.java
@@ -2,4 +2,36 @@ package lezione_20251022_maven_e_git;
 
 public class Studente {
 
+	
+	private String matricola;
+	private String nome;
+	private String cognome;
+	public String getMatricola() {
+		return matricola;
+	}
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public Studente(String matricola, String nome, String cognome) {
+		super();
+		this.matricola = matricola;
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	
+	
+	
+	
 }
