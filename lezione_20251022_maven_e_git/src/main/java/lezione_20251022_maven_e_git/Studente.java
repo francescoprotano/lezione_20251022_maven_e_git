5c91871c7ebd384590a80910109d7a9d35f312a8
Exolab <f.protano@exolab.it>
2025-10-22 11:02:13 +0200

    commit n.3

--- lezione_20251022_maven_e_git/src/main/java/lezione_20251022_maven_e_git/Studente.java
+++ lezione_20251022_maven_e_git/src/main/java/lezione_20251022_maven_e_git/Studente.java
@@ -30,6 +30,21 @@ public class Studente {
 		this.nome = nome;
 		this.cognome = cognome;
 	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 	
 	
 	
